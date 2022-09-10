b9cd4626b2ef1eadbc68a53a34c698b0c64a11ef
Mayeda <cassidy.mayeda@vanderbilt.edu>
2022-09-10 09:58:19 -0700

    Add separate function file

--- R/TREX.R
+++ R/TREX.R
@@ -1,3 +1,20 @@
-TREX <- function(data1, data2, kvalue = 60,  ) {
+TREX <- function(data1, data2,                     # dataframes from two sets for comparison 
+                 kvalue = 60,                      # number, for KNN search 
+                 bins = 5                          # number of bins to use for TREX 
+                 ) {
+  
+  # run KNN 
+  
+  # bin data 
+  
+  # plot output 
+  
+  # save & export plot as PNG
+  
+  # calc degree of change 
+  
+  # save & export degree of change as CSV
+  # also num cells, and direction of change 
+  
   
 }
