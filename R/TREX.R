170010e471bb3f64483bd3ebba3c73f61c7f2619
Mayeda <cassidy.mayeda@vanderbilt.edu>
2022-09-12 13:53:12 -0700

    Allow specification of output file. TREX_cluster() can export marker data for clusters.

--- R/TREX.R
+++ R/TREX.R
@@ -11,7 +11,7 @@ TREX <- function(embedding.data,                   # dataframe, 2 columns of low
   
   binned.data = TREX_bin(embedding.data, kvalue, bins)
   
-  # TREX_plot(binned.data)
+  TREX_plot(binned.data)
 
   TREX_stats(binned.data)
   
@@ -60,7 +60,7 @@ TREX_plot <- function(binned.data) {
       values = trex.colors) +
     guides(color = guide_legend(override.aes = list(size = 4))) +
     labs(x = "UMAP1", y = "UMAP2",
-         title = paste(sample_type,"_",sample_id,"_",time_comparison," - Percent Change",sep = ""),
+         # title = paste(sample_type,"_",sample_id,"_",time_comparison," - Percent Change",sep = ""),
          caption = "Data from Brodin lab in Rodriguez et al., Cell Rep Med. 2020") +
     theme_bw() +
     theme(panel.grid.major = element_blank(),
@@ -69,7 +69,7 @@ TREX_plot <- function(binned.data) {
   
   # save & export plot as PNG
   ggsave(
-    paste0(strftime(Sys.time(), "%Y-%m-%d_%H%M"), "_TREX_plot.png"), 
+    paste0(strftime(Sys.time(), "%Y-%m-%d_%H_%M"), "_TREX_plot.png"), 
     width = 8, 
     height = 8
   )
@@ -80,10 +80,10 @@ TREX_stats <- function(binned.data) {
 
   sample.table <- data.frame(total_cells = nrow(binned.data))
   sums = c(sum(binned.data$cuts == '(15,85)'),
-                    sum(binned.data$cuts == '(5,15]'),
-                    sum(binned.data$cuts == '[0,5]'),
-                    sum(binned.data$cuts == '[85,95)'),
-                    sum(binned.data$cuts == '[95,100]'))
+           sum(binned.data$cuts == '(5,15]'),
+           sum(binned.data$cuts == '[0,5]'),
+           sum(binned.data$cuts == '[85,95)'),
+           sum(binned.data$cuts == '[95,100]'))
   
   sample.table[, c(2:6)] <- sums
   colnames(sample.table)[2:6] <- c("(15,85)","(5,15]","[0,5]","[85,95)","[95,100]")
@@ -93,31 +93,35 @@ TREX_stats <- function(binned.data) {
   sample.table$direction_of_change = (sums[5] - sums[3]) / (sums[5] + sums[3])
   
   # export stats as CSV
-  write.csv(sample.table, paste0(strftime(Sys.time(),"%Y-%m-%d_%H%M"), "_TREX_stats.csv"))
+  write.csv(sample.table, paste0(strftime(Sys.time(),"%Y-%m-%d_%H_%M"), "_TREX_stats.csv"))
   
   return(sample.table) 
 }
 
 # use DBSCAN to cluster on regions of great change (5th and 95 percentiles of change)
-TREX_cluster <- function(binned.data) {
+TREX_cluster <- function(binned.data,                   # output from TREX() or TREX_bin()
+                         feature.data = data.frame()    # same nrow as binned.data, not used in any computations
+                                                        # optional, used only to export cluster data for downstream use 
+                         ) {
 
-  regions.of.interest <- binned.data %>%
-    dplyr::filter(cuts == "[0,5]" | cuts == "[95,100]")
+  if (nrow(feature.data) > 0) {
+    all.data <- cbind(binned.data, feature.data) 
+  } else {
+    all.data <- binned.data 
+  }
   
+  regions.of.interest <- all.data %>%
+    dplyr::filter(cuts == "[0,5]" | cuts == "[95,100]")
+
   db.result = dbscan::dbscan(regions.of.interest[, 1:2], eps = 0.3, minPts = 1)
   track.data = cbind(regions.of.interest, cluster = db.result$cluster)
   track.data <- track.data %>%
     filter(cluster != 0)
   
-  # --- do these lines do anything? 
-  # track.data$cluster[which(track.data$status=="[0,5]")] <- paste0(track.data$cluster[which(track.data$status=="[0,5]")],"05")
-  # track.data$cluster[which(track.data$status=="[95,100]")] <- paste0(track.data$cluster[which(track.data$status=="[95,100]")],"95")
-  # track.data$cluster<-as.numeric(track.data$cluster)
-  
   cluster.data = split(track.data, track.data$cluster)
   median.percent.change = lapply(cluster.data, function(x) median(x[, which(colnames(track.data)=="percent.change")]))
   mean.percent.change = lapply(cluster.data, function(x) mean(x[, which(colnames(track.data)=="percent.change")]))
-  write.csv(mean.percent.change, paste0(strftime(Sys.time(),"%Y-%m-%d_%H%M"),"_cluster_ave_percent_change.csv"))
+  write.csv(mean.percent.change, paste0(strftime(Sys.time(),"%Y-%m-%d_%H_%M"),"_cluster_ave_percent_change.csv"))
 
   qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
   col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
@@ -145,7 +149,7 @@ TREX_cluster <- function(binned.data) {
 
   # save & export plot as PNG
   ggsave(
-    paste0(strftime(Sys.time(), "%Y-%m-%d_%H%M"), "_DBSCAN_plot.png"), 
+    paste0(strftime(Sys.time(), "%Y-%m-%d_%H_%M"), "_DBSCAN_plot.png"), 
     width = 8, 
     height = 8
   )
