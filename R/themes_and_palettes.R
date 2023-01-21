128423852f4975dcd0dd8871be96e900d80e20f6
cmayeda <cassidymayeda@gmail.com>
2023-01-20 17:25:12 -0800

    Move 'get' fns to helpers.R. re-write bin generation from # or %

--- R/themes_and_palettes.R
+++ R/themes_and_palettes.R
@@ -1,49 +1,17 @@
 library (tidyverse)
 library(RColorBrewer)
 
-get_percent_labels <- function(bin.levels, data.names) {
-  
-  bin.labels = vector()
-  for (bin in bin.levels) {
-    num.list = strsplit(bin, split = ",")
-    first.num = num.list[[1]][1]
-    second.num = num.list[[1]][2]
-    
-    # check for decimal points 
-    if (str_detect(first.num, "\\.")) {
-      first.num <- as.numeric(str_extract(first.num, "[0-9]+\\.[0-9]+"))
-    } else {
-      first.num <- as.numeric(str_extract(first.num, "[0-9]+"))
-    }
-    if (str_detect(second.num, "\\.")) {
-      second.num <- as.numeric(str_extract(second.num, "[0-9]+\\.[0-9]+"))
-    } else {
-      second.num <- as.numeric(str_extract(second.num, "[0-9]+"))
-    }
-    
-    if (first.num == 0) {
-      cluster.label = paste0("\u2265", (100 - second.num), "% from ", data.names[1])
-    } else if (second.num == 100) {
-      cluster.label = paste0("\u2265", first.num, "% from ", data.names[2])
-    } else if (second.num <= 50 & first.num <= 50) {
-      cluster.label = paste0((100 - second.num), "-", (100 - first.num), "% from ", data.names[1]) 
-    } else if (second.num > 50 & first.num > 50) {
-      cluster.label = paste0(first.num, "-", second.num, "% from ", data.names[2])
-    } else {
-      cluster.label = paste0("from ", data.names[1], " and ", data.names[2])
-    }
-    bin.labels <- append(bin.labels, cluster.label)
-  }
-  return(bin.labels)
-}
-
 default_colors <- c("navyblue","lightskyblue","lightgray","lightcoral","darkred")
 thirteen_colors <- c("navyblue","#3E6BCE","lightskyblue","#D6F3FC","#AEC2CC",
                      "#79878E","#5D5D5D","#8E7979","#CCAEAE","#FFDEDE",
                      "lightcoral","#C45555","darkred")
 
 get_TREX_colors <- function(bins) {
-  if (length(bins) == 5) {
+  if (length(bins) == 3) {
+    bin.colors = default_colors[c(1,3,5)]
+  } else if (length(bins) == 4) {
+    bin.colors = default_colors[c(1,2,4,5)]
+  } else if (length(bins) == 5) {
     bin.colors = default_colors
   } else if(length(bins) > 5 & length(bins) < 13) {
     bin.colors = colorRampPalette(default_colors)(length(bins))
@@ -53,6 +21,7 @@ get_TREX_colors <- function(bins) {
     library(RColorBrewer)
     bin.colors = colorRampPalette(thirteen_colors)(length(bins))
   }
+  
   names(bin.colors) <- bins
   return(bin.colors)
 }
